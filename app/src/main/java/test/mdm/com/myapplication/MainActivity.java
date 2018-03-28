e2e382c7e4e746b12acec8d8e8e1c59ce82a34be
Vincentvik <vincent.vik@portal724.com>
2018-03-28 14:55:28 +0530

    commit2

--- app/src/main/java/test/mdm/com/myapplication/MainActivity.java
+++ app/src/main/java/test/mdm/com/myapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Branch_01
+        // Branch_01_commit2
     }
 }
